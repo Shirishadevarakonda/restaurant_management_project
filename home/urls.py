f557501f088a36c7dc6437f56f3a477d324dae33
Shirisha <shirisha2004q@gmail.com>
2025-08-15 16:47:14 +0530

    33521

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('about/',views.about,name='about'),
+    path('about/',views.about_view,name='about'),
     
 ]
