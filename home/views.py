ca23404090c10df5e0a46beeec785692aeb3b987
Shirisha <shirisha2004q@gmail.com>
2025-08-19 16:39:11 +0530

    33520

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.shortcuts import render
 def home_view(request):
-    name=getattr(settings,"RESTAURANT_NAME","Spice Graden")
+    name=getattr(settings,"RESTAURANT_NAME","Spice Garden")
 
-    return render(request,'home.html',{restauant_name":name})
+    return render(request,'home.html',{restauant_name":restaurant_name})
     
 
 # Create your views here.
